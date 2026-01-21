5d3f6c452680bc7987cbaeafdfdeac0905fd9129
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-01-21 17:26:28 -0500

    added some words that are placeholders but now working on the js

---
+++ words.js
@@ -0,0 +1,27 @@
+let words = [
+    {
+        words: "labyrinth",
+        hint: "A complicated and confusing arrangement. You might find it in a puzzle or a maze."
+    },
+
+    {
+        words: "epiphany",
+        hint: "A moment of clarity or insight."
+    },
+
+    {
+        words: "luminous",
+        hint: "Emitting light or full of light."
+    },
+
+    {
+        words: "javascript",
+        hint: "The most popular programming language."
+    },
+
+    {
+        words: "solitude",
+        hint: "The state of being alone that can be peaceful and enjoyable."
+    },
+
+]
